3a4dec31f71461b45f111a14ac723376496c744e
Christian Heilmann <chris.heilmann@gmail.com>
2022-05-13 14:46:17 +0200

    Fixing event listener

--- bml-code.js
+++ bml-code.js
@@ -4,10 +4,11 @@
   s.innerHTML=`body::before{content:'✏️ Edit Mode (ESC to end)';z-index:64;padding:1em;background:white;color:black;display:block;margin:1em;font-size:30px;border:5px solid green;}`;
   document.body.appendChild(s);
   window.scrollTo(0,0);
-  document.addEventListener('keyup',e=>{
+  document.addEventListener('keyup',e => {
     if(e.key==='Escape'){
       document.designMode='off';
       s.remove();
+      document.removeEventListener('keyup',e);
     }
-  },{once:true});
+  });
 })();
